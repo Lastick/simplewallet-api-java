3dd31c5e067c28580645b1220e5bdaa9ed8c2d5d
Sasha <Sasha@titan-machine.pp.ua>
2017-09-27 18:02:16 +0300

    Add methods

--- SimpleWallet.java
+++ SimpleWallet.java
@@ -1,4 +1,4 @@
-package club.karbo.lightwallet;
+package club.karbo.karbolightwallet;
 
 // Copyright (c) 2017 The Karbowanec developers
 //
@@ -16,6 +16,8 @@ package club.karbo.lightwallet;
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import java.io.IOException;
+import java.security.SecureRandom;
+import java.text.DecimalFormat;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
@@ -26,6 +28,7 @@ import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import android.annotation.SuppressLint;
 import android.util.Log;
 
 
@@ -35,7 +38,7 @@ public class SimpleWallet {
   private final int port_default = 15000;
   private final String rpc_v = "2.0";
   private final String id_conn = "EWF8aIFX0y9w";
-  private final String log_tag = "LightWallet";
+  private final String log_tag = "KarboLightWallet";
   private String host = null;
   private int port = 0;
   private Boolean service_status = false;
@@ -72,17 +75,15 @@ public class SimpleWallet {
     return buff;
   }
 
-  public Double[] getBalance(){
-	Double[] result = new Double[2];
-    result[0] = 0.0;
-    result[1] = 0.0;
+  public String getAddress(){
+	String result = "";
     String buff = "";
     JSONObject args = new JSONObject();
     this.oper_status = false;
     try {
       args.put("jsonrpc", this.rpc_v);
       args.put("id", this.id_conn);
-      args.put("method", "getbalance");
+      args.put("method", "getaddress");
 	  } catch (JSONException e){
       e.printStackTrace();
 	}
@@ -90,11 +91,12 @@ public class SimpleWallet {
     if (this.service_status){
       try {
         JSONObject root = new JSONObject(buff);
-        if (root.getString("id").equals(this.id_conn)){
-          JSONObject res = root.getJSONObject("result");
-          result[0] = res.getDouble("available_balance");
-          result[1] = res.getDouble("locked_amount");
-          this.oper_status = true;
+        if (root.isNull("error")){
+          if (root.getString("id").equals(this.id_conn)){
+            JSONObject res = root.getJSONObject("result");
+            result = res.getString("address");
+            this.oper_status = true;
+          }
         }
 	    } catch (JSONException e){
 		e.printStackTrace();
@@ -124,10 +126,12 @@ public class SimpleWallet {
     if (this.service_status){
       try {
         JSONObject root = new JSONObject(buff);
-        if (root.getString("id").equals(this.id_conn)){
-          JSONObject res = root.getJSONObject("result");
-          result = res.getInt("height");
-          this.oper_status = true;
+        if (root.isNull("error")){
+          if (root.getString("id").equals(this.id_conn)){
+            JSONObject res = root.getJSONObject("result");
+            result = res.getInt("height");
+            this.oper_status = true;
+          }
         }
 	    } catch (JSONException e){
 		e.printStackTrace();
@@ -141,9 +145,219 @@ public class SimpleWallet {
     return result;
   }
 
+  public void doReset(){
+    String buff = "";
+    JSONObject args = new JSONObject();
+    this.oper_status = false;
+    try {
+      args.put("jsonrpc", this.rpc_v);
+      args.put("id", this.id_conn);
+      args.put("method", "reset");
+	  } catch (JSONException e){
+      e.printStackTrace();
+	}
+    buff = this.doServiceWallet(args.toString());
+    Log.d(this.log_tag, buff);
+    if (this.service_status){
+      try {
+        JSONObject root = new JSONObject(buff);
+        if (root.isNull("error")){
+          if (root.getString("id").equals(this.id_conn)){
+            this.oper_status = true;
+          }
+        }
+	    } catch (JSONException e){
+		e.printStackTrace();
+      }
+    }
+    if (this.oper_status){
+      Log.d(this.log_tag, "wallet: success");
+      } else {
+      Log.d(this.log_tag, "wallet: fail");
+    }
+  }
 
+  public long[] getBalance(){
+    long[] result = new long[2];
+    result[0] = 0;
+    result[1] = 0;
+    String buff = "";
+    JSONObject args = new JSONObject();
+    this.oper_status = false;
+    try {
+      args.put("jsonrpc", this.rpc_v);
+      args.put("id", this.id_conn);
+      args.put("method", "getbalance");
+	  } catch (JSONException e){
+      e.printStackTrace();
+	}
+    buff = this.doServiceWallet(args.toString());
+    if (this.service_status){
+      try {
+        JSONObject root = new JSONObject(buff);
+        if (root.isNull("error")){
+          if (root.getString("id").equals(this.id_conn)){
+            JSONObject res = root.getJSONObject("result");
+            result[0] = res.getLong("balance");
+            result[1] = res.getLong("unlocked_balance");
+            this.oper_status = true;
+          }
+        }
+	    } catch (JSONException e){
+		e.printStackTrace();
+      }
+    }
+    if (this.oper_status){
+      Log.d(this.log_tag, "wallet: success");
+      } else {
+      Log.d(this.log_tag, "wallet: fail");
+    }
+    return result;
+  }
+  
+  public Object[][] getTransfers(){
+    Object[][] result = new Object[0][0];
+    String buff = "";
+    JSONObject args = new JSONObject();
+    this.oper_status = false;
+    try {
+      args.put("jsonrpc", this.rpc_v);
+      args.put("id", this.id_conn);
+      args.put("method", "get_transfers");
+      } catch (JSONException e){
+      e.printStackTrace();
+    }
+    buff = this.doServiceWallet(args.toString());
+    if (this.service_status){
+      try {
+        JSONObject root = new JSONObject(buff);
+        if (root.isNull("error")){
+          if (root.getString("id").equals(this.id_conn)){
+            JSONObject res = root.getJSONObject("result");
+            if (res.has("transfers")){
+              JSONArray transfers = res.getJSONArray("transfers");
+              int size = transfers.length();
+              if(size > 0){
+                result = new Object[size][9];
+                for (int i = 0; i < size; i++){
+                  Log.d(this.log_tag, "i: " + i);
+                  JSONObject items_obj = transfers.getJSONObject(i);
+                  result[i][0] = new String("");
+                  if (items_obj.has("address")){
+                    result[i][0] = new String(items_obj.getString("address"));
+                  }
+                  result[i][1] = (long) 0;
+                  if (items_obj.has("amount")){
+                    result[i][1] = (long) items_obj.getLong("amount");
+                  }
+                  result[i][2] = (int) 0;
+                  if (items_obj.has("blockIndex")){
+                    result[i][2] =(int) items_obj.getInt("blockIndex");
+                  }
+                  result[i][3] = (long) 0;
+                  if (items_obj.has("fee")){
+                    result[i][3] = (long) items_obj.getLong("fee");
+                  }
+                  result[i][4] = (Boolean) false;
+                  if (items_obj.has("output")){
+                    result[i][4] = (Boolean) items_obj.getBoolean("output");
+                  }
+                  result[i][5] = new String("");
+                  if (items_obj.has("paymentId")){
+                    result[i][5] = new String(items_obj.getString("paymentId"));
+                  }
+                  result[i][6] = (int) 0;
+                  if (items_obj.has("time")){
+                    result[i][6] = (int) items_obj.getInt("time");
+                  }
+                  result[i][7] = new String("");
+                  if (items_obj.has("transactionHash")){
+                    result[i][7] = new String(items_obj.getString("transactionHash"));
+                  }
+                  result[i][8] = (int) 0;
+                  if (items_obj.has("unlockTime")){
+                    result[i][8] = (int) items_obj.getInt("unlockTime");
+                  }
+                }
+              }
+            }
+            this.oper_status = true;
+          }
+        }
+      } catch (JSONException e){
+        e.printStackTrace();
+      }
+    }
+    if (this.oper_status){
+      Log.d(this.log_tag, "wallet: success");
+      } else {
+      Log.d(this.log_tag, "wallet: fail");
+    }
+    return result;
+  }
+
+  public String getSeed(){
+    String result = "";
+    String buff = "";
+    JSONObject args = new JSONObject();
+    JSONObject params = new JSONObject();
+    this.oper_status = false;
+    try {
+      args.put("jsonrpc", this.rpc_v);
+      args.put("id", this.id_conn);
+      args.put("method", "query_key");
+      params.put("key_type", "mnemonic");
+      args.put("params", params);
+      } catch (JSONException e){
+      e.printStackTrace();
+    }
+    buff = this.doServiceWallet(args.toString());
+    if (this.service_status){
+      try {
+        JSONObject root = new JSONObject(buff);
+        if (root.isNull("error")){
+          if (root.getString("id").equals(this.id_conn)){
+            JSONObject res = root.getJSONObject("result");
+              result = res.getString("key");
+              this.oper_status = true;
+            }
+          }
+        } catch (JSONException e){
+        e.printStackTrace();
+      }
+	}
+	if (this.oper_status){
+      Log.d(this.log_tag, "wallet: success");
+      } else {
+	  Log.d(this.log_tag, "wallet: fail");
+	}
+    return result;
+  }
+  
   public Boolean getOperStatus(){
     return this.oper_status;
+  }
+
+  @SuppressLint("DefaultLocale")
+  public static String AmountToString(long amount){
+    String res = "";
+    DecimalFormat df = new DecimalFormat("#.00##########");
+    res = df.format(amount * 0.000000000001);
+    return res;
+  }
+
+  @SuppressLint("TrulyRandom")
+  public static String genPaymentId(){
+    String res = "";
+    SecureRandom random = new SecureRandom();
+    byte bytes[] = new byte[32];
+    random.nextBytes(bytes);
+    StringBuffer result = new StringBuffer();
+    for (byte b : bytes){
+      result.append(String.format("%02X", b));
+    }
+    res = result.toString();
+    return res;
   }
 
 }
